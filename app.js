f6467c47af7759d9de6d152f4a1e4a32d266e5b0
Eric Mickelson <ericmick@mac.com>
2015-10-27 17:03:39 -0700

    Simple Nodejs example.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var pg = require('pg')
-var retry = require('retry')
+var pg = require('pg');
+var retry = require('retry');
 
-console.log("Hello World!")
+console.log("Hello World!");
 
-var operation = retry.operation({retries:3})
+var operation = retry.operation({retries:3});
 
 operation.attempt(function() {
-  var client = new pg.Client()
+  var client = new pg.Client();
   client.connect(function(e) {
-    client.end()
+    client.end();
     if(operation.retry(e)) {
       return;
     }
-    if(!e) console.log("Hello Postgres!")
-  })
-})
+    if(!e) console.log("Hello Postgres!");
+  });
+});
 
